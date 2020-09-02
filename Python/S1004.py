f3735148e3f0b000a5a960623c9343adc5d1bc02
irostub <irostub@gmail.com>
2020-09-03 08:44:11 +0900

    Update formatting to codestyle black

--- Python/S1004.py
+++ Python/S1004.py
@@ -1,4 +1,5 @@
 import math
+
 for _ in range(int(input())):
     posInput = tuple(map(int, input().split()))
     startPos = posInput[:2]
@@ -9,11 +10,18 @@ for _ in range(int(input())):
     count = 0
     for planet in planets:
         startPointDist = math.sqrt(
-            math.pow(startPos[0] - planet[0], 2)+math.pow(startPos[1]-planet[1], 2))
+            math.pow(startPos[0] - planet[0], 2) + math.pow(startPos[1] - planet[1], 2)
+        )
         endPointDist = math.sqrt(
-            math.pow(endPos[0] - planet[0], 2)+math.pow(endPos[1]-planet[1], 2))
+            math.pow(endPos[0] - planet[0], 2) + math.pow(endPos[1] - planet[1], 2)
+        )
         if startPointDist < planet[2] and endPointDist < planet[2]:
             pass
-        elif startPointDist < planet[2] and endPointDist > planet[2] or startPointDist > planet[2] and endPointDist < planet[2]:
+        elif (
+            startPointDist < planet[2]
+            and endPointDist > planet[2]
+            or startPointDist > planet[2]
+            and endPointDist < planet[2]
+        ):
             count += 1
     print(count)
